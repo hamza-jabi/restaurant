3dc2ad7f1998a99a12a0425dfc1d9dfb72a914e8
hamza jabi <s12216885@stu.najah.edu>
2025-12-18 14:55:37 +0000

    add docker file

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,16 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
-scriptDir=$(cd "$(dirname "$0")" && pwd)
-cd "$scriptDir/../backend"
+
+mkdir data
 
 if [ ! -f "data/restaurants.db" ]
 then
     echo "Database not found. Initializing..."
-    if [ ! -d "data" ]
-    then
-        mkdir data
-    fi
     python3 init_db.py
 fi
 
